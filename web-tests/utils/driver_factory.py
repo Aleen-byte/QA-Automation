f6b96cce8a54b61fde20f77e6f1dde8059ab832d
Théo Alencar <theoalen123@gmail.com>
2026-05-07 10:03:18 -0300

    correcoes do webtests

--- web-tests/utils/driver_factory.py
+++ web-tests/utils/driver_factory.py
@@ -1,5 +1,8 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 
 def create_driver(headless=True):
@@ -8,7 +11,16 @@ def create_driver(headless=True):
         options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
+    options.add_argument("--remote-debugging-port=0")
 
-    # Selenium Manager (built into Selenium 4.6+) handles ChromeDriver automatically
+    chrome_bin = os.environ.get("CHROME_BIN")
+    if chrome_bin:
+        options.binary_location = chrome_bin
+
+    chromedriver_path = os.environ.get("CHROMEDRIVER_PATH")
+    if chromedriver_path:
+        return webdriver.Chrome(service=Service(executable_path=chromedriver_path), options=options)
+
     return webdriver.Chrome(options=options)
